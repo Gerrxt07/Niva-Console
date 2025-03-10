f98fca1bb799fceaa24127d11e97b92d4d37516a
Gerrxt <gerrxt07@proton.me>
2025-03-10 16:16:59 +0100

    Remove language support files and related initialization code

--- Niva.py
+++ Niva.py
@@ -1,12 +1,3 @@
 import Scripts.Core.Startup as Startup
-from Scripts.Language.Language import language_loader, translate
-
-# Initialize language settings
-language_loader.load_config()
-language_loader.load_translations()
-
-print(translate("welcome_message"))
 
 Startup.main()
-
-print(translate("exit_message"))
